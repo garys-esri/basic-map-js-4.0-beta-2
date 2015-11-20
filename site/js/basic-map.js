9fcb47f2cae243e29c3cfed922b9309945a5b3ba
Gary Sheppard <gsheppard@esri.com>
2015-11-20 17:22:27 -0500

    Toggling between vector tile styles

--- site/js/basic-map.js
+++ site/js/basic-map.js
@@ -1,6 +1,10 @@
+var layers = [];
+var visibleLayerIndex = 0;
+
 require([
   "esri/Map",
   "esri/views/MapView",
+  "dojo/parser", "dijit/form/Button",
   "dojo/domReady!"
 ], function(Map, MapView) {
 
@@ -14,23 +18,34 @@ require([
   });
   require([
     "esri/Basemap",
-    "esri/layers/VectorTileLayer",
-    "esri/layers/ArcGISTiledLayer"
+    "esri/layers/VectorTileLayer"
   ], function(
       Basemap,
-      VectorTileLayer,
-      ArcGISTiledLayer
+      VectorTileLayer
   ) {
+    var primaryStyle = "https://www.arcgis.com/sharing/rest/content/items/f96366254a564adda1dc468b447ed956/resources/styles/root.json";
+    var secondaryStyle = "http://www.arcgis.com/sharing/rest/content/items/3b8814f6ddbd485cae67e8018992246e/resources/styles/root.json";
+    var primaryLayer = new VectorTileLayer({ url: primaryStyle });
+    layers.push(primaryLayer);
+    var secondaryLayer = new VectorTileLayer({ url: secondaryStyle, visible: false });
+    layers.push(secondaryLayer);
     var defaultBasemap = new Basemap({
-      baseLayers: [
-        //new VectorTileLayer({ url: "https://basemaps.arcgis.com/arcgis/rest/services/World_Basemap/VectorTileServer" })
-        new VectorTileLayer({ url: "https://www.arcgis.com/sharing/rest/content/items/f96366254a564adda1dc468b447ed956/resources/styles/root.json" })
-      ],
+      baseLayers: [ primaryLayer, secondaryLayer ],
       title: "Default",
       id: "default"
     });
     map.basemap = defaultBasemap;
-    
   });
 
 });
+
+function basemapButtonClicked(val) {
+  visibleLayerIndex++;
+  if (layers.length <= visibleLayerIndex) {
+    visibleLayerIndex = 0;
+  }
+  var i = 0;
+  for (; i < layers.length; i++) {
+    layers[i].visible = (i == visibleLayerIndex);
+  }
+}
